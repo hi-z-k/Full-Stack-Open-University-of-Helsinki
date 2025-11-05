35d9893869fdd1f2a4687ac64dad47af4f70e4e8
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-11-05 18:48:06 +0300

    feat(part-1): FSO unicafe Task 1.7 DONE
    - meets all the requirements under unicafe step2
    Task 1.7

--- Part-1-React-basics/1.6-unicafe/src/App.jsx
+++ Part-1-React-basics/1.6-unicafe/src/App.jsx
@@ -7,16 +7,29 @@ const Header = ({ content }) => {
   return <h1>{content}</h1>;
 };
 const StatDisplay = ({ good, neutral, bad }) => {
-  return <>
-  <p>good {good}</p>
-  <p>neutral {neutral}</p>
-  <p>bad {bad}</p>
-  </>;
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const positive = (good / total)*100;
+  return (
+    <>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>total {total}</p>
+      {total > 0 && (
+        <>
+          <p>avarage {avarage}</p>
+          <p>positive {positive}%</p>
+        </>
+      )}
+    </>
+  );
 };
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
   const handleGoodFeedback = () => {
     setGood(good + 1);
   };
@@ -32,8 +45,8 @@ const App = () => {
       <Feedback name={"Good"} onComment={handleGoodFeedback} />
       <Feedback name={"Neutral"} onComment={handleNeutralFeedback} />
       <Feedback name={"Bad"} onComment={handleBadFeedback} />
-      <Header content={"statisitcs"} />
-      <StatDisplay good={good} neutral={neutral} bad={bad}/>
+      <Header content={"statistics"} />
+      <StatDisplay good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
