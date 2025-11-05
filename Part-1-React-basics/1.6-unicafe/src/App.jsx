4e2b357e09eee7c02365aa2c3b8c0e75bc3c5121
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-11-05 20:05:13 +0300

    feat(part-1): FSO unicafe Task 1.9 DONE
    - meet all the requirements under unicafe step4
    - the requirements for step 3 were already met

--- Part-1-React-basics/1.6-unicafe/src/App.jsx
+++ Part-1-React-basics/1.6-unicafe/src/App.jsx
@@ -9,18 +9,20 @@ const Header = ({ content }) => {
 const StatDisplay = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = (good - bad) / total;
-  const positive = (good / total)*100;
+  const positive = (good / total) * 100;
   return (
     <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>total {total}</p>
-      {total > 0 && (
+      {total > 0 ? (
         <>
-          <p>avarage {avarage}</p>
-          <p>positive {positive}%</p>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>total {total}</p>
+          <p>average {average}</p>
+          <p>positive {positive} %</p>
         </>
+      ) : (
+        <p>No feedback given</p>
       )}
     </>
   );
