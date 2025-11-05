01a9458bc24b917163c016a80c130caf4a0c693d
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-11-05 20:29:55 +0300

    feat(part-1): FSO unicafe project DONE
    - meet all the requirements under step5 and step6
    (Task 1.10 & 1.11)

--- Part-1-React-basics/1.6-unicafe/src/App.jsx
+++ Part-1-React-basics/1.6-unicafe/src/App.jsx
@@ -1,26 +1,37 @@
 import { useState } from "react";
 
-const Feedback = ({ name, onComment }) => {
+const Button = ({ name, onComment }) => {
   return <button onClick={onComment}>{name}</button>;
 };
 const Header = ({ content }) => {
   return <h1>{content}</h1>;
 };
-const StatDisplay = ({ good, neutral, bad }) => {
+
+const StatisticLine = ({ text, value, end = "" }) => (
+  <tr className="data-row">
+    <td>{text}</td>
+    <td>
+      {value} {end}
+    </td>
+  </tr>
+);
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = (good - bad) / total;
   const positive = (good / total) * 100;
   return (
     <>
       {total > 0 ? (
-        <>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>total {total}</p>
-          <p>average {average}</p>
-          <p>positive {positive} %</p>
-        </>
+        <table>
+          <tbody>
+            <StatisticLine text={"good"} value={good} />
+            <StatisticLine text={"neutral"} value={neutral} />
+            <StatisticLine text={"bad"} value={bad} />
+            <StatisticLine text={"total"} value={total} />
+            <StatisticLine text={"average"} value={average} />
+            <StatisticLine text={"positive"} value={positive} end="%" />
+          </tbody>
+        </table>
       ) : (
         <p>No feedback given</p>
       )}
@@ -44,11 +55,11 @@ const App = () => {
   return (
     <div>
       <Header content={"give feedback"} />
-      <Feedback name={"Good"} onComment={handleGoodFeedback} />
-      <Feedback name={"Neutral"} onComment={handleNeutralFeedback} />
-      <Feedback name={"Bad"} onComment={handleBadFeedback} />
+      <Button name={"Good"} onComment={handleGoodFeedback} />
+      <Button name={"Neutral"} onComment={handleNeutralFeedback} />
+      <Button name={"Bad"} onComment={handleBadFeedback} />
       <Header content={"statistics"} />
-      <StatDisplay good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
