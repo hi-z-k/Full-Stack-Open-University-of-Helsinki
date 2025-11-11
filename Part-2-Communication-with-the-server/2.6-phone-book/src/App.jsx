d3899b318affb6bfb6c4eed70e22d9f934b2e041
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-11-11 18:29:51 +0300

    feat(part-2): FSO - phone book task 2.6 DONE
    - create an AddressList Component
    - save current user input
    - save name to list upon submission

---
+++ Part-2-Communication-with-the-server/2.6-phone-book/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const AddressList = ({data})=>{
+  return (<div>
+    {data.map((user)=><p key={user.name}>{user.name}</p>)}
+  </div>)
+}
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+  const handleNewName = (e)=>{
+    setNewName(e.target.value)
+  }
+  const submitPerson = (e)=>{
+    e.preventDefault()
+    setPersons(persons.concat({name:newName}))
+    setNewName('')  
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={submitPerson}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handleNewName}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <AddressList data={persons}/>
+    </div>
+  )
+
+}
+
+export default App
