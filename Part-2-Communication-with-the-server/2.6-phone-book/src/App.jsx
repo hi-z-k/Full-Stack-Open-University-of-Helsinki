c39b9f0e10f29a3b8b55c9d9c030267ec438a43a
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-11-11 19:21:08 +0300

    feat(part-2): FSO - phone book task 2.7 DONE
    - prevent duplicate entries
    - the checker is case-insensetive

--- Part-2-Communication-with-the-server/2.6-phone-book/src/App.jsx
+++ Part-2-Communication-with-the-server/2.6-phone-book/src/App.jsx
@@ -1,45 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const AddressList = ({data})=>{
-  return (<div>
-    {data.map((user)=><p key={user.name}>{user.name}</p>)}
-  </div>)
-}
-
+const AddressList = ({ data }) => {
+  return (
+    <div>
+      {data.map((user) => (
+        <p key={user.name}>{user.name}</p>
+      ))}
+    </div>
+  );
+};
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  
-  const handleNewName = (e)=>{
-    setNewName(e.target.value)
-  }
-  const submitPerson = (e)=>{
-    e.preventDefault()
-    setPersons(persons.concat({name:newName}))
-    setNewName('')  
-  }
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNewName = (e) => {
+    setNewName(e.target.value);
+  };
+  const submitPerson = (e) => {
+    e.preventDefault();
+    const included = persons.some((user) => user.name.toLowerCase() === newName.toLowerCase());
+    if (included) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={submitPerson}>
         <div>
-          name: <input 
-          value={newName}
-          onChange={handleNewName}
-          />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <AddressList data={persons}/>
+      <AddressList data={persons} />
     </div>
-  )
+  );
+};
 
-}
-
-export default App
+export default App;
