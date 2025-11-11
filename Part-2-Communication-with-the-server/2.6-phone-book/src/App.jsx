db9cea046edd4dd15653d3e8a0931b4c1908f1dd
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-11-11 20:39:12 +0300

    feat(part-2): FSO - phone book task 2.10 DONE
    - refactored the code by separating
    the 3 components from the application

--- Part-2-Communication-with-the-server/2.6-phone-book/src/App.jsx
+++ Part-2-Communication-with-the-server/2.6-phone-book/src/App.jsx
@@ -1,76 +1,105 @@
 import { useState } from "react";
 
-const AddressList = ({ data }) => {
+const Persons = ({ data }) => {
   return (
     <div>
       {data.map((user) => (
-        <p key={user.name}>{user.name}: {user.phone}</p>
+        <p key={user.name}>
+          {user.name}: {user.phone}
+        </p>
       ))}
     </div>
   );
 };
 
+const PersonForm = ({ action }) => {
+  return (<form onSubmit={action.submit[0]}>
+    <div>
+      Name: <input value={action.name[0]} onChange={action.name[1]} />
+    </div>
+    <div>
+      Phone Number:{" "}
+      <input value={action.phone[0]} onChange={action.phone[1]} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>)
+};
+
+const Filter = ({action})=>{
+  return <div>
+        Search: <input value={action.query[0]} onChange={action.query[1]} />
+      </div>
+}
+
 const App = () => {
-const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456' },
-    { name: 'Ada Lovelace', phone: '39-44-5323523' },
-    { name: 'Dan Abramov', phone: '12-43-234345' },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
-  ])  
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-123456" },
+    { name: "Ada Lovelace", phone: "39-44-5323523" },
+    { name: "Dan Abramov", phone: "12-43-234345" },
+    { name: "Mary Poppendieck", phone: "39-23-6423122" },
+  ]);
+
   const [newName, setNewName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("+251")
-  const [query, setQuery] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState("+251");
+  const [query, setQuery] = useState("");
+
   const handleNewName = (e) => {
     setNewName(e.target.value);
   };
   const handlePhoneNumber = (e) => {
     setPhoneNumber(e.target.value);
-  }
-  const handleQuery = (e) => {
-    setQuery(e.target.value)
-  }
-  const queryResult = (data) => {
-    return data.filter(person => {
-      const name = person.name.toLowerCase()
-      const queryTerm = query.toLowerCase()
-      return name.includes(queryTerm)
-    })
-  }
-  let searchResult = persons
-  if (query !== ""){
-    searchResult = queryResult(persons)
-  }
-
+  };
   const submitPerson = (e) => {
     e.preventDefault();
-    const included = persons.some((user) => user.name.toLowerCase() === newName.toLowerCase());
+    const included = persons.some(
+      (user) => user.name.toLowerCase() === newName.toLowerCase()
+    );
     if (included) {
-      alert(`${newName} is already added to phonebook`)
-      return
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
-    setPersons(persons.concat({
-      name: newName,
-      phone: phoneNumber
-    }));
-    setNewName("")
-    setPhoneNumber("+251")
+    setPersons(
+      persons.concat({
+        name: newName,
+        phone: phoneNumber,
+      })
+    );
+    setNewName("");
+    setPhoneNumber("+251");
   };
+  
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+  };
+  
+  const queryResult = (data) => {
+    return data.filter((person) => {
+      const name = person.name.toLowerCase();
+      const queryTerm = query.toLowerCase();
+      return name.includes(queryTerm);
+    });
+  };
+  let searchResult = persons;
+  if (query !== "") {
+    searchResult = queryResult(persons);
+  }
+  
+  const formAction = {
+    name: [newName, handleNewName],
+    phone: [phoneNumber, handlePhoneNumber],
+    query: [query, handleQuery],
+    submit: [submitPerson]
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>Search: <input value={query} onChange={handleQuery} /></div>
-      <form onSubmit={submitPerson}>
-      <div>
-        Name: <input value={newName} onChange={handleNewName} />
-      </div>
-        <h2>Add a new</h2>
-        <div>Phone Number: <input value={phoneNumber} onChange={handlePhoneNumber} /></div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter action={formAction}/>
+      <h2>Add a new</h2>
+      <PersonForm action={formAction} />
       <h2>Numbers</h2>
-      <AddressList data={searchResult} />
+      <Persons data={searchResult} />
     </div>
   );
 };
