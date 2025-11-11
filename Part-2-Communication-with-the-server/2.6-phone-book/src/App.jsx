4b2cc7b62cc9f3b807a525e2d7182a4b3829e394
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-11-11 20:01:31 +0300

    feat(part-2): FSO - phone book task 2.9 DONE
    - implement a search bar that filters from the data

--- Part-2-Communication-with-the-server/2.6-phone-book/src/App.jsx
+++ Part-2-Communication-with-the-server/2.6-phone-book/src/App.jsx
@@ -11,16 +11,36 @@ const AddressList = ({ data }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", phone:"+251901020304" }]);
+const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
+  ])  
   const [newName, setNewName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("+251")
-
+  const [query, setQuery] = useState('')
   const handleNewName = (e) => {
     setNewName(e.target.value);
   };
-  const handlePhoneNumber = (e)=>{
+  const handlePhoneNumber = (e) => {
     setPhoneNumber(e.target.value);
   }
+  const handleQuery = (e) => {
+    setQuery(e.target.value)
+  }
+  const queryResult = (data) => {
+    return data.filter(person => {
+      const name = person.name.toLowerCase()
+      const queryTerm = query.toLowerCase()
+      return name.includes(queryTerm)
+    })
+  }
+  let searchResult = persons
+  if (query !== ""){
+    searchResult = queryResult(persons)
+  }
+
   const submitPerson = (e) => {
     e.preventDefault();
     const included = persons.some((user) => user.name.toLowerCase() === newName.toLowerCase());
@@ -28,9 +48,9 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat({ 
-      name: newName, 
-      phone:phoneNumber 
+    setPersons(persons.concat({
+      name: newName,
+      phone: phoneNumber
     }));
     setNewName("")
     setPhoneNumber("+251")
@@ -38,17 +58,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Search: <input value={query} onChange={handleQuery} /></div>
       <form onSubmit={submitPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNewName} />
-        </div>
-        <div>number: <input value={phoneNumber} onChange={handlePhoneNumber}/></div>
+      <div>
+        Name: <input value={newName} onChange={handleNewName} />
+      </div>
+        <h2>Add a new</h2>
+        <div>Phone Number: <input value={phoneNumber} onChange={handlePhoneNumber} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <AddressList data={persons} />
+      <AddressList data={searchResult} />
     </div>
   );
 };
