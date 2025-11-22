c2441f8c992ea2d913e3699d1e7d1b2586ea0b1c
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-11-22 10:47:53 +0300

    feat(part-3): FSO - phonebook backend task 3.3 DONE
    - get data by its id from phonebook

--- Part-3-Server-programming-with-NodeJS's-Express-library/3.1-phonebook-backend/index.js
+++ Part-3-Server-programming-with-NodeJS's-Express-library/3.1-phonebook-backend/index.js
@@ -1,41 +1,48 @@
-import express, { response } from "express";
+import express, { request, response } from "express";
 
 const app = express();
 const PORT = 3001;
-const persons =[
-    {
-      "name": "Arto Hellas",
-      "phone": "040-123456",
-      "id": "1"
-    },
-    {
-      "name": "Ada Lovelace",
-      "phone": "39-44-5323523",
-      "id": "2"
-    },
-    {
-      "name": "Dan Abramov",
-      "phone": "12-43-234345",
-      "id": "3"
-    },
-    {
-      "name": "Mary Poppendieck",
-      "phone": "39-23-6423122",
-      "id": "4"
-    }
-  ]
-app.get('/api/persons',(request,response)=>{
-    response.json(persons)
+const persons = [
+  {
+    "name": "Arto Hellas",
+    "phone": "040-123456",
+    "id": "1"
+  },
+  {
+    "name": "Ada Lovelace",
+    "phone": "39-44-5323523",
+    "id": "2"
+  },
+  {
+    "name": "Dan Abramov",
+    "phone": "12-43-234345",
+    "id": "3"
+  },
+  {
+    "name": "Mary Poppendieck",
+    "phone": "39-23-6423122",
+    "id": "4"
+  }
+]
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
-app.get('/info',(request,response)=>{
-    let message = `
+app.get('/info', (request, response) => {
+  let message = `
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
     `
-    response.type('html').send(message)
+  response.type('html').send(message)
 })
 
 
-app.listen(PORT,()=>{
-    console.log(`Notes API is live at Port ${PORT}`)
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const data = persons.find(n => n.id === id)
+  if (data) response.json(data)
+  else response.status(404).send("Person not found in the phonebook")
+})
+
+app.listen(PORT, () => {
+  console.log(`Notes API is live at Port ${PORT}`)
 })
