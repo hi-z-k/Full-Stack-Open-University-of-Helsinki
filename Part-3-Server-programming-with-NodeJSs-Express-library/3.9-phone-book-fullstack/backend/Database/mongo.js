7155566f212467e434c2e0f14c2cc32542d0ed76
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2025-12-29 01:59:26 +0300

    feat(part-3):phonebook FSO Task 3.15 - 3.18 DONE
    - delete person data by id
    - error handling centralized on one error handler
    - update person phone by using name as key
    - provide resource as specified in assignment
    - sends 404 for invalid route requests

--- Part-3-Server-programming-with-NodeJSs-Express-library/3.9-phone-book-fullstack/backend/Database/mongo.js
+++ Part-3-Server-programming-with-NodeJSs-Express-library/3.9-phone-book-fullstack/backend/Database/mongo.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import 'dotenv/config'
+import "dotenv/config";
 import Person from "./Schema/Person.js";
 
 const URL = process.env.URL;
@@ -31,23 +31,28 @@ const getPhonebook = () => {
 
 const addPerson = (name, phone) => {
   if (!name || !phone) {
-    return Promise.reject({scode:400,data:"Data incomplete"});
+    return Promise.reject({ scode: 400, data: "Data incomplete" });
   }
   return startConnection()
     .then(() => exists(name))
     .then((exist) => {
       if (exist) {
-        return Promise.reject({scode:409,data:"The person already in database"});
+        return Promise.reject({
+          scode: 409,
+          data: "The person already in database",
+        });
       }
       const person = new Person({
         name,
         phone,
       });
-      return person.save()
-      .then(data=>({scode:201,data}))
+      return person.save().then((data) => ({ scode: 201, data }));
     })
     .catch((e) => {
-        return Promise.reject({scode:500,data:`unable to register ${name} to the phonebook`});
+      return Promise.reject({
+        scode: 500,
+        data: `unable to register ${name} to the phonebook`,
+      });
     });
 };
 const exists = (name) => {
@@ -57,58 +62,91 @@ const exists = (name) => {
         name,
       })
     )
-    .catch((e) => {
-      console.log("searching data failed");
-      throw e;
+    .then(person=>{
+      if (!person) {
+        return Promise.reject({
+          scode: 404,
+          data: "The person not in phonebook",
+        });
+      }
+      return person
+    })
+    .catch((error) => {
+      if (error.scode) {
+        return Promise.reject(error);
+      }
+      return Promise.reject({ scode: 500, data: `finding person ${name} failed` });
     });
 };
 
-const findPerson = (id)=>{
-    return startConnection()
-    .then(() =>
-      Person.findById(id)
-    )
-    .then(person=>{
-        if(!person){
-            return Promise.reject({scode:404,data:"The person not in phonebook"});
-        }
-        return {scode:200,data:person}
+const findPerson = (id) => {
+  return startConnection()
+    .then(() => Person.findById(id))
+    .then((person) => {
+      if (!person) {
+        return Promise.reject({
+          scode: 404,
+          data: "The person not in phonebook",
+        });
+      }
+      return { scode: 200, data: person };
     })
-    .catch(({name}) => {
-        if(name=="CastError"){
-            return Promise.reject({scode:400,data:"Invalid Id"});
-        }
-      return Promise.reject({scode:500,data:"searching person failed"});
+    .catch(({ name }) => {
+      if (name == "CastError") {
+        return Promise.reject({ scode: 400, data: "Invalid Id" });
+      }
+      return Promise.reject({ scode: 500, data: "searching person failed" });
     });
-}
+};
 
-const deletePerson = (id)=>{
-    return startConnection()
-    .then(() =>
-      Person.findByIdAndDelete(id)
-    )
-    .then(person=>{
-        if(!person){
-            return Promise.reject({scode:404,data:"The person not in phonebook"});
-        }
-        return {scode:204,data:null}
+const updatePerson = (name, phone) => {
+  if (!name || !phone) {
+    return Promise.reject({ scode: 400, data: "Data incomplete" });
+  }
+  return exists(name)
+    .then((person) => {
+      person.phone = phone;
+      return person.save();
     })
-    .catch(({name}) => {
-        if(name=="CastError"){
-            return Promise.reject({scode:400,data:"Invalid Id"});
-        }
-      return Promise.reject({scode:500,data:"deleting person failed"});
+    .then((data) => ({ scode: 200, data }))
+    .catch((error) => {
+      if (!error.scode) {
+        return Promise.reject({ scode: 500, data: `updating ${name}'s phone number failed` });
+      }
+      return Promise.reject(error);
     });
-}
+};
 
+const deletePerson = (id) => {
+  return startConnection()
+    .then(() => Person.findByIdAndDelete(id))
+    .then((person) => {
+      if (!person) {
+        return Promise.reject({
+          scode: 404,
+          data: "The person not in phonebook",
+        });
+      }
+      return { scode: 204, data: null };
+    })
+    .catch(({ name }) => {
+      if (name == "CastError") {
+        return Promise.reject({ scode: 400, data: "Invalid Id" });
+      }
+      return Promise.reject({ scode: 500, data: "deleting person failed" });
+    });
+};
 
-const total = ()=>{
-    return startConnection()
+const total = () => {
+  return startConnection()
     .then(() => Person.countDocuments({}))
-    .then(count=>({scode:200,data:count}))
+    .then((count) => ({ scode: 200, data: count }))
     .catch((e) => {
-      return Promise.reject({scode:500,data:"counting people on phonebook failed"});
+      return Promise.reject({
+        scode: 500,
+        data: "counting people on phonebook failed",
+      });
     });
-}
+};
 
-export { getPhonebook, addPerson,findPerson, total,deletePerson };
+export { getPhonebook, updatePerson, addPerson, findPerson, total, deletePerson };
--- Part-3-Server-programming-with-NodeJSs-Express-library/3.9-phone-book-fullstack/backend/index.js
+++ Part-3-Server-programming-with-NodeJSs-Express-library/3.9-phone-book-fullstack/backend/index.js
@@ -1,10 +1,11 @@
-import express from "express";
+import express, { response } from "express";
 import morgan from "morgan";
 import {
   getPhonebook,
   addPerson,
   findPerson,
   deletePerson,
+  updatePerson,
   total,
 } from "./Database/mongo.js";
 import "dotenv/config";
@@ -24,43 +25,72 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(morgan(`${tiny} :content`));
 
-app.get("/api/persons", (request, response) => {
-  getPhonebook().then((persons) => response.json(persons));
+app.get("/api/persons", (request, response,next) => {
+  getPhonebook()
+  .then((persons) => response.json(persons))
+  .catch(error => next(error))
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response,next) => {
   const id = request.params.id;
-  findPerson(id).then(({ scode, data }) => {
-    response.status(scode).json(data);
-  });
+  findPerson(id)
+  .then(({ scode, data }) => {
+    response.status(scode).json(data)
+  })
+  .catch(error => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response,next) => {
   const data = request.body;
   const { name, phone } = data;
-  addPerson(name, phone).then(({ scode, data }) => {
+  addPerson(name, phone)
+  .then(({ scode, data }) => {
     response.status(scode).json(data);
-  });
+  })
+  .catch(error => next(error));
 });
 
-app.get("/info", async (request, response) => {
+app.get("/info", async (request, response,next) => {
   try {
     const { scode, data } = await total();
     response.status(scode).type("html").send(`
       <p>Phonebook has info for ${data} people</p>
       <p>${new Date()}</p>
       `);
-  } catch (e) {
-    response.status(500).send("Something went wrong");
-  }
+  } catch (error) {
+      next(error)
+    }
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response,next) => {
   const id = request.params.id;
   deletePerson(id).then(({ scode, data }) => {
     response.status(scode).json(data);
-  });
+  })
+  .catch(error => next(error));
 });
+
+
+app.put("/api/persons", (request, response,next) => {
+  const {name,phone} = request.body;
+  updatePerson(name,phone).then(({ scode, data }) => {
+    response.status(scode).json(data);
+  })
+  .catch(error => next(error));
+});
+
+
+app.use((request,response,next)=>{
+  response.status(404).send({
+    error: "the resource doesn't exist"
+  })
+})
+
+const errorHandler = (error,request,response,next) =>{
+  response.status(error.scode || 500).json({ error: error.data })
+}
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Phonebook API is live at Port ${PORT}`);
