94de13795dd361bce785adbfd673f804e6c23489
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2026-01-05 01:22:54 +0300

    feat(part-4):blog-list FSO Task 4.1 DONE
    - setup the server for blogpost
    - integration to remote cluster

---
+++ Part-4-Express-application-testing-user-management/4.1-blog-list/index.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+const MONGO_URL = process.env.MONGO_URL
+const PORT = process.env.PORT
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(MONGO_URL, { family: 4 })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
