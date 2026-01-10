1a277b35e09fc0247342a819aff0184b27767ec3
Hizkeal <104999321+hi-z-k@users.noreply.github.com.>
2026-01-11 01:15:22 +0300

    feat(part-4):blog-list FSO Task 4.4 DONE
    - implement total likes and include tests

--- Part-4-Express-application-testing-user-management/4.1-blog-list/tests/list_helper.test.js
+++ Part-4-Express-application-testing-user-management/4.1-blog-list/tests/list_helper.test.js
@@ -1,9 +1,26 @@
 import { test, describe }  from 'node:test'
 import assert from 'node:assert'
-import {dummy} from '../utils/list_helper.js'
+import {dummy, totalLikes} from '../utils/list_helper.js'
+import {blogs,listWithOneBlog} from './blogs.js'
 
 test('dummy returns one', () => {
   const blogs = []
   const result = dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+  test('when list has many blogs',()=>{
+    const result = totalLikes(blogs)
+    assert.strictEqual(result,36)
+  })
+  test('when list have no blog',()=>{
+    const result = totalLikes([])
+    assert.strictEqual(result, 0)
+  })
 })
--- Part-4-Express-application-testing-user-management/4.1-blog-list/utils/list_helper.js
+++ Part-4-Express-application-testing-user-management/4.1-blog-list/utils/list_helper.js
@@ -1,5 +1,11 @@
 const dummy = blogs => 1
 
+const totalLikes = blogs => {
+    let likesCount = blogs.reduce((count,blog)=>count+=blog.likes, 0)
+    return likesCount
+}
+
 export{
-    dummy
+    dummy,
+    totalLikes
 }
