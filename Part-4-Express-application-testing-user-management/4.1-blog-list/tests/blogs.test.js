8023e6f58149342a1a949d2e78a80ca348e0bb11
Hizkeal <hizkealyeshitilab@gmail.com>
2026-04-06 13:06:04 +0300

    feat(part-4):blog-list FSO Task 4.9 DONE
    - write a test to check
       - check if the unique identifier is named id instead of _id
       - check if database is the one generating the id

--- Part-4-Express-application-testing-user-management/4.1-blog-list/tests/blogs.test.js
+++ Part-4-Express-application-testing-user-management/4.1-blog-list/tests/blogs.test.js
@@ -5,6 +5,7 @@ import app from '../app.js'
 import supertest from 'supertest'
 import { initialBlogs } from './initialBlogs.js'
 import Blog from '../models/blog.js'
+import { info } from '../utils/logger.js'
 
 const api = supertest(app)
 
@@ -23,6 +24,20 @@ describe('Tests on Blog Router',()=>{
         .expect('Content-Type', /application\/json/)
         const blogCount = response.body.length 
         assert.strictEqual(blogCount, initialBlogs.length)
+    })
+    test.only('The unique identifier for the blog is named id',async ()=>{
+        const response = await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+        const blog = response.body[0]
+        const content = initialBlogs[0]
+        const isIdCreated = 
+                "id" in blog &&
+                !("id" in content) &&
+                !("_id" in blog) 
+
+        assert.strictEqual(isIdCreated, true)
     })
 })
 after(async () => {
