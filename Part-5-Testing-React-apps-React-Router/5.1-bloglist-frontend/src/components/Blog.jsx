16822d41e4a7f02cd4a57d1c2ff189edff5007bd
Hizkeal <hizkealyeshitilab@gmail.com>
2026-04-22 23:58:37 +0300

    feat(bloglist-frontend): FSO Task 5.7 DONE
    - give Blog style
    - give blog a view/hide button to show/hide extra info

--- Part-5-Testing-React-apps-React-Router/5.1-bloglist-frontend/src/components/Blog.jsx
+++ Part-5-Testing-React-apps-React-Router/5.1-bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [viewAll, setViewAll] = useState(false)
+  const handleViewAll = ()=>setViewAll(!viewAll)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={handleViewAll}>{viewAll?"view":"hide"}</button>
+      {viewAll && <>
+      <div>{blog.url}</div>
+      <div>likes {blog.likes}</div>
+      <div>{blog.user.name}</div>
+      </>}
+      
+    </div>
+  )
+}
+
 
 export default Blog
