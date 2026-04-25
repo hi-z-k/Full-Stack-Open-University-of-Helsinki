69b7f6bdc9746591bdff1ec74960dbf6e33846cf
Hizkeal <hizkealyeshitilab@gmail.com>
2026-04-26 01:10:53 +0300

    feat(bloglist-test): FSO Task 5.14 DONE
    - implement a test to check if blog's view button is clicked, it
    displays URL and number of Likes

--- Part-5-Testing-React-apps-React-Router/5.1-bloglist-frontend/src/components/Blog.unit.test.jsx
+++ Part-5-Testing-React-apps-React-Router/5.1-bloglist-frontend/src/components/Blog.unit.test.jsx
@@ -1,26 +1,46 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
+const blog = {
+  'title': 'Brave New World',
+  'author': 'Aldous Huxley',
+  'url': 'https://www.huxley.net/bnw/one.html',
+  'likes': 6213,
+  'user': {
+    'name': 'Lucrezia Borgia',
+    'username': 'lborgia',
+    'id': '1'
+  },
+  'id': '2'
+}
+
 test('Blog renders title and author only by default',() => {
-  const blog = {
-    'title': 'Brave New World',
-    'author': 'Aldous Huxley',
-    'url': 'https://www.huxley.net/bnw/one.html',
-    'likes': 6213,
-    'user': {
-      'name': 'Lucrezia Borgia',
-      'username': 'lborgia',
-      'id': '1'
-    },
-    'id': '2'
-  }
   render(<Blog data={{ blog, user:blog.user }}/>)
   const titleAndAuthor = screen.getByText(`${blog.title} ${blog.author}`)
   const url = screen.queryByText(blog.url)
   const likes = screen.queryByText(blog.likes)
 
-
   expect(titleAndAuthor).toBeDefined()
   expect(url).toBeNull()
   expect(likes).toBeNull()
+})
+
+test('Blog shows URL & likes when "view" button is clicked', async() => {
+  render(<Blog data={{ blog, user:blog.user }}/>)
+  let url = screen.queryByText(blog.url)
+  let likes = screen.queryByText(blog.likes)
+
+  expect(url).toBeNull()
+  expect(likes).toBeNull()
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  url = screen.getByText(blog.url)
+  likes = screen.getByText(blog.likes, { exact: false })
+
+  expect(url).toBeDefined()
+  expect(likes).toBeDefined()
 })
