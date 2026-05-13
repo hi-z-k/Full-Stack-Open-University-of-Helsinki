cf4ab039f59830ad4192505e3e35011bdd913ebc
Hizkeal <hizkealyeshitilab@gmail.com>
2026-05-13 03:25:08 +0300

    feat(zustand-anecdotes): FSO Task 6.8 DONE
    - implement add anecdotes with fetch API

--- Part-6-Advanced-State-Management/6.2-zustand-anecdotes/src/services/anecdotes.js
+++ Part-6-Advanced-State-Management/6.2-zustand-anecdotes/src/services/anecdotes.js
@@ -1,11 +1,32 @@
 const baseUrl = 'http://localhost:3001/anecdotes'
 
-const getAll = async()=>{
+const getAll = async () => {
     const response = await fetch(baseUrl)
-    if (!response.ok){
+    if (!response.ok) {
         throw new Error('Failed to fetch anecdotes')
     }
     return await response.json()
 }
 
-export default { getAll }
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const anecdoteBuilder = anecdote => ({
+    content: anecdote,
+    id: getId(),
+    votes: 0
+})
+const createAnecdote = async (anecdote) => {
+    const newAnecdote = anecdoteBuilder(anecdote)
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newAnecdote)
+    }
+    const response = await fetch(baseUrl, options)
+    if (!response.ok) {
+        throw new Error('Failed to create anecdotes')
+    }
+    return await response.json()
+}
+
+export default { getAll, createAnecdote }
--- Part-6-Advanced-State-Management/6.2-zustand-anecdotes/src/store.js
+++ Part-6-Advanced-State-Management/6.2-zustand-anecdotes/src/store.js
@@ -1,15 +1,6 @@
-
 import { create } from 'zustand'
 import anecdoteService from './services/anecdotes.js'
 
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = anecdote => ({
-  content: anecdote,
-  id: getId(),
-  votes: 0
-})
 
 const useAnecdoteStore = create((set) => ({
   anecdotes: [],
@@ -17,7 +8,7 @@ const useAnecdoteStore = create((set) => ({
   actions: {
     initialize: async ()=>{
       const anecdotes = await anecdoteService.getAll()
-      set(()=>({ anecdotes }))
+      set(()=>({ anecdotes: anecdotes.toSorted((a, b) => b.votes - a.votes) }))
     },
     vote: id => set(state => ({
       anecdotes: state.anecdotes.map(anecdote =>
@@ -25,8 +16,8 @@ const useAnecdoteStore = create((set) => ({
       ).toSorted((a, b) => b.votes - a.votes)
     })
     ),
-    add: anecdote => {
-      const newAnecdote = asObject(anecdote)
+    add: async (anecdote) => {
+      const newAnecdote = await anecdoteService.createAnecdote(anecdote)
       return set(
         state => ({ anecdotes: state.anecdotes.concat(newAnecdote).toSorted((a, b) => b.votes - a.votes) })
   )},
