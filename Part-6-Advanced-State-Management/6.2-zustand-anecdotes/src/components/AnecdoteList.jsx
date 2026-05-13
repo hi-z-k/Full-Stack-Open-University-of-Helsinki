b6cfae261ea49a44d5d028faa7057a3d4cba2b3d
Hizkeal <hizkealyeshitilab@gmail.com>
2026-05-13 03:14:55 +0300

    feat(zustand-anecdotes): FSO Task 6.7 DONE
    - implement initialization with fetch  API

--- Part-6-Advanced-State-Management/6.2-zustand-anecdotes/src/components/AnecdoteList.jsx
+++ Part-6-Advanced-State-Management/6.2-zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -1,24 +1,28 @@
 
+import { useEffect } from 'react'
 import { useAnecdotes, useAnecdoteActions } from '../store'
 
 const AnecdoteList = () => {
   const anecdotes = useAnecdotes()
-  const { vote } = useAnecdoteActions()
+  const { vote, initialize } = useAnecdoteActions()
 
+  useEffect(() => {
+    initialize()
+  }, [initialize])
   return (
-  <div>
+    <div>
       {anecdotes.map(anecdote => (
         <div key={anecdote.id}>
-            <div>{anecdote.content}</div>
-            <div>
+          <div>{anecdote.content}</div>
+          <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
-            </div>
+          </div>
         </div>
-        ))}
-  </div>
+      ))}
+    </div>
   )
-  
+
 }
 
 export default AnecdoteList
