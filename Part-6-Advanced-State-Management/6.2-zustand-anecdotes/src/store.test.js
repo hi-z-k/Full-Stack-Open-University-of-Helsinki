e515de0585a26097f2f56e2798136544ca2fc0f7
Hizkeal <hizkealyeshitilab@gmail.com>
2026-05-16 01:05:08 +0300

    test(zustand-anecdotes): FSO Task 6.13 DONE
    - test if anecdotes received by the components are sorted
    by their votes descendingly

--- Part-6-Advanced-State-Management/6.2-zustand-anecdotes/src/store.test.js
+++ Part-6-Advanced-State-Management/6.2-zustand-anecdotes/src/store.test.js
@@ -19,25 +19,43 @@ beforeEach(() => {
 })
 
 describe('using the anecdotes actions', () => {
-    it('initialize anecdotes from service', async () => {
-        const mockAnecdotes = [
-            {
-                "content": "Adding manpower to a late software project makes it later!",
-                "id": "21149",
-                "votes": 10
-            },
-            {
-                "content": "If it hurts, do it more often",
-                "id": "47145",
-                "votes": 7
-            }
-        ]
+    const mockAnecdotes = [
+        {
+            "content": "Adding manpower to a late software project makes it later!",
+            "id": "21149",
+            "votes": 10
+        },
+        {
+            "content": "If it hurts, do it more often",
+            "id": "47145",
+            "votes": 7
+        }
+    ]
+    const initializeAnecdotes = async (mockAnecdotes)=>{
         anecdoteService.getAll.mockResolvedValue(mockAnecdotes)
-        const { result: {current: { initialize }} } = renderHook(()=>useAnecdoteActions())
-
+        const { result: { current: { initialize } } } = renderHook(() => useAnecdoteActions())
         await act(async () => initialize())
-
-        const { result: { current: anecdotes } } = renderHook(()=>useAnecdotes())
+    }
+    beforeEach(async() => {
+        await initializeAnecdotes(mockAnecdotes)
+    })
+    it('initialize anecdotes from service', async () => {
+        const { result: { current: anecdotes } } = renderHook(() => useAnecdotes())
         expect(anecdotes).toEqual(mockAnecdotes)
+    })
+    it('the anecdotes are sorted by votes descendingly',async()=>{
+        await initializeAnecdotes(mockAnecdotes.toReversed())
+        const { result: { current: anecdotes } } = renderHook(() => useAnecdotes())
+        let isDescending = true
+        let prev = anecdotes[0]
+        for (let i = 1; i < anecdotes.length; i++) {
+            const curr = anecdotes[i];
+            if (prev.votes < curr.votes){
+                isDescending = false
+                break
+            }
+            prev = curr
+        }
+        expect(isDescending).toBe(true)
     })
 })
