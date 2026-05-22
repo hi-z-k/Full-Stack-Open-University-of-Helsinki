86a60bc8b4b09d92e78aaca32ce2791b9ad497c6
Hizkeal <hizkealyeshitilab@gmail.com>
2026-05-22 03:59:57 +0300

    feat(routed-anecdotes): FSO Task 7.3-7.5 DONE
    - solution already avoids the warning(7.3)
    - implment fetching anecdotes with useAnecdotes hook(7.4)
    - implment adding anecdotes with useAnecdotes hook(7.5)

--- Part-7-Custom-Hooks-Esbuild/7.1-routed-anecdotes/src/hooks/index.js
+++ Part-7-Custom-Hooks-Esbuild/7.1-routed-anecdotes/src/hooks/index.js
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import anecdoteService from '../services/anecdotes'
+
 
 export const useField = (type) => {
     const [value, setValue] = useState('')
@@ -12,5 +14,33 @@ export const useField = (type) => {
             onChange
         },
         reset: () => setValue('')
+    }
+}
+
+export const useAnecdotes = () => {
+    const [anecdotes, setAnecdotes] = useState([])
+    useEffect(() => {
+        const getAnecdotes = async () => {
+            try {
+                const data = await anecdoteService.getAll()
+                setAnecdotes(data)
+            } catch (error) {
+                console.error('Failed to fetch anecdotes:', error)
+            }
+        }
+        getAnecdotes()
+    }, [])
+    const addAnecdotes = async (anecdote) => {
+            try {
+                const newAnecdote = await anecdoteService.createNew({...anecdote, id: Math.round(Math.random() * 10000) })
+                setAnecdotes(anecdotes.concat(newAnecdote))
+            } catch (error) {
+                console.error('Failed to add anecdotes:', error)
+            }
+        }
+
+    return {
+        anecdotes,
+        addAnecdotes
     }
 }
